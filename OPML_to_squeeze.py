f1a353e9c0b385db14c79258cb529a2bb417ae67
Oliver Gondring <oliver.gondring@uni-passau.de>
2016-08-26 14:46:13 +0200

    Commend Line Parameters and Usage Information

--- OPML_to_squeeze.py
+++ OPML_to_squeeze.py
@@ -4,32 +4,37 @@
 # sudo pip install selenium listparser
 # sudo apt-get install phantomjs
 
+# TODO: take credentials and opml path as command line parameters
+
+import sys
 import time
 import listparser as lp
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-dcap = dict(DesiredCapabilities.PHANTOMJS)
+# Evaluate CL Arguments and Display Usage Information
 
-# user specific variables
-msb_user = '<USER NAME>'
-msb_pass = '<PASSWORD>'
+if len(sys.argv) < 3:
+	print ("Usage: python OPML_to_squeeze.py [Username] [Password] [OPML file to import]")
+	quit()
 
-# location of OPML file to import
-opml_path = "<OPML FILE PATH>"
+# User specific variables
+msb_user = sys.argv[1]
+msb_pass = sys.argv[2]
 
+# Location of local OPML file to import
+opml_path = sys.argv[3]
+
+# mysqueezebox.com URL (the redirect saves us one step)
 URL = 'http://mysqueezebox.com/user/login?redirect=user/apps'
 
+# Start Browser and load mysqueezebox.com
+dcap = dict(DesiredCapabilities.PHANTOMJS)
 driver = webdriver.PhantomJS(desired_capabilities=dcap)
-# driver = webdriver.Firefox()
-
 driver.get(URL)
 
-driver.save_screenshot('squeeeze.png')
-
-# Anmeldung
-# (evtl. gar nicht notwendig)
+# Login on mysqueezebox.com
 element = driver.find_element_by_xpath('//*[@id="email"]')
 element.send_keys(msb_user)
 
@@ -37,50 +42,36 @@ element = driver.find_element_by_xpath('//*[@id="password"]')
 element.send_keys(msb_pass)
 
 element.submit()
-# ---
 
-# Kunstpause
+# Wait for page to load
 time.sleep(1)
 
-# Eintraege löschen
+# Delete current podcast subscription list
 
-# Zum Löschen eines Eintrages muss folgende URL mit der entsprechenden Nummer aufgerufen werden:
-# http://mysqueezebox.com/settings/podcasts/delete/[Nummer]
+# The URL scheme for deleting entries in the list is
+# http://mysqueezebox.com/settings/podcasts/delete/[ID]
 
-# Liste der IDs generieren
+# Generate list of IDs
 podcasts = driver.find_elements_by_xpath('//ol/li')
 pod_ids = []
 for pod in podcasts:
 	pod_ids.append(pod.get_attribute("id").replace("draggable_","",1))
 
-# Kontrolle
-print pod_ids
-
-# alle löschen
+# Send them to /dev/null
 for pod in pod_ids:
-	time.sleep(0.5) # Pause, WICHTIG wegen Ladezeit des Browsers
+	# important time delay for server response
+	time.sleep(0.5)
 	driver.get("http://mysqueezebox.com/settings/podcasts/delete/" + pod)
 
-# lokale OPML-Datei laden
+# Load local OPML file
 opml_file = open(opml_path)
 opml_cont = opml_file.read()
 pods = lp.parse(opml_cont)
 
+# Create new subscription list, one entry at a time
 for feed in pods.feeds:
 	element = driver.find_element_by_xpath('//input[@name="url"]')
 	element.clear()
 	element.send_keys(feed.url)
 	driver.find_element_by_xpath('//*[@id="add_button"]').click()
 	time.sleep(0.5)
-
-# Squeezebox OPML holen
-# opml_link = driver.find_element_by_partial_link_text("OPML").get_attribute("href")
-# print opml_link
-# opml_file = driver.get(opml_link)
-# xPATH to SQL: "select the "href" attribute (@) of an "a" tag that appears anywhere (//), but only where (the bracketed phrase) the textual contents of the "a" tag contain 'Podcasts als OPML'".
-
-# falls der Link statisch ist, kann einfach folgender Ausdruck verwendet werden
-# driver.get("http://mysqueezebox.com/public/opml/ecd807dcd498c2ff71d4637c8a0bc3aa546671ec/podcasts.opml")
-
-
-# loeschen und neu generieren
